aa4e184e45f0160b513bd60cbafb5153b080a1d5
priyadarsini-nerella <heydisispriya@hotmail.com>
2026-04-02 17:30:57 +0530

    Refactor main.py and add example to predict function

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ def home():
     return {"message": "California House Price Prediction API is running in docker!"}
 
 @app.post("/predict")
-def predict(features: list[float]):
+def predict(features: list[float]= Body(
+    ...,
+    example=[8.3252, 41, 6.984, 1.023, 322, 2.555, 37.88, -122.23]
+)):
     prediction = model.predict([features])
     return {"predicted_price": float(prediction[0])}
 #python -m uvicorn app:app --reload
